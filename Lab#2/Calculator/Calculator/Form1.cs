73d6ec1f2d151233740a6ce9685888bebe261425
ascripnic <scripnic.alexandru@gmail.com>
2017-01-11 23:40:52 +0200

    add basic ui and core data

---
+++ Lab#2/Calculator/Calculator/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Core;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        private Calculator calc = new Calculator();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addDigit(object sender, EventArgs e)
+        {
+            displayLabel.addDigitOrDot((sender as Button).Text);
+        }
+
+        private void clear(object sender, EventArgs e)
+        {
+            displayLabel.resetLabel();
+            calc.value = 0;
+        }
+
+        private void delete(object sender, EventArgs e)
+        {
+            displayLabel.deleteLastChar();
+        }
+
+        private void toggleSign(object sender, EventArgs e) {
+            displayLabel.toggleSign();
+        }
+
+        private void setExpression(object sender, EventArgs e) {
+            calc.setExpression((sender as Button).Text);
+            displayLabel.resetLabel();
+        }
+    }
+}
---
+++ Lab#2/Core/Core/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Calculator : Operations
+    {
+        private string expression;
+
+        public void setExpression(string value)
+        {
+            expression = value;
+        }
+
+        private void setValue(double value)
+        {
+            this.value = value;
+        }
+
+        public void executeExpression(double value)
+        {
+            switch (expression)
+            {
+                case "√": sqrt(value); break;
+                case "x^y": power(value); break;
+                case "÷": mod(value); break;
+                case "×": multiply(value); break;
+                case "-": substract(value); break;
+                case "+": sum(value); break;
+                default: setValue(value); break;
+            }
+        } 
+    }
+}
