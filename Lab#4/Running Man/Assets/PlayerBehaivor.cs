4f4d1c21fefadb1e2b2df2c99a02482b40ea85b8
ascripnic <scripnic.alexandru@gmail.com>
2017-01-23 18:21:48 +0200

    add animations to game and add mobile devices controls

--- Lab#4/Running Man/Assets/PlayerBehaivor.cs
+++ Lab#4/Running Man/Assets/PlayerBehaivor.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaivor : MonoBehaviour {
     private Rigidbody2D playerRigidbody;
     private float lastSpeedPower;
     private float speedBoost = 1.1f;
+    private float getUpTime;
 
     public float CharacterSpeed;
+    public float SlideDuration;
     public float JumpPower = 40;
     public GameMechanics GameMaster;
     public LayerMask Ground;
+    public LayerMask Obstacle;
 
+    private Vector2 startPos;
+    private Vector2 direction;
+    private bool directionChosen;
     private bool grounded;
+    private bool sliding;
+    private bool gameOver = false;
 
 	void Start () {
         playerRigidbody = GetComponent<Rigidbody2D>();
@@ -22,31 +31,99 @@ public class PlayerBehaivor : MonoBehaviour {
     }
 	
 	void Update () {
-        if (lastSpeedPower != CharacterSpeed) {
+        if (!gameOver) {
             SetVelocity(CharacterSpeed, playerRigidbody.velocity.y);
+        } else if (Input.GetMouseButtonDown(0) || Input.touchCount > 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
         if (Time.timeSinceLevelLoad  / GameMaster.NextIncreaseAt > 1) {
             CharacterSpeed *= speedBoost;
         }
 
-        Movement();
+        if (Physics2D.IsTouchingLayers(GetComponent<BoxCollider2D>(), Obstacle)) {
+            CharacterSpeed = 0;
+            JumpPower = 0;
+            gameOver = true;
+
+            SetVelocity(0, 0);
+            GetComponent<Rigidbody2D>().AddForce((Vector3.left + Vector3.up) * 30, ForceMode2D.Impulse);
+        }
+
+        if (getUpTime < Time.timeSinceLevelLoad) {
+            slide(false);
+        }
+
+        keyboardMovement();
+        touchMovement();
+
+        GetComponent<Animator>().SetBool("grounded", grounded);
+        GetComponent<Animator>().SetBool("sliding", sliding);
+        GetComponent<Animator>().SetFloat("speed", CharacterSpeed);
     }
 
-    private void Movement() {
+    private void touchMovement() {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            
+            switch (touch.phase) {
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    directionChosen = false;
+                    break;
+                    
+                case TouchPhase.Moved:
+                    direction = touch.position - startPos;
+                    break;
+                    
+                case TouchPhase.Ended:
+                    directionChosen = true;
+                    break;
+            }
+        }
+
+        if (directionChosen) {
+            directionChosen = false;
+
+            if (direction.y < 0) {
+                getUpTime = Time.timeSinceLevelLoad + SlideDuration;
+                slide(true);
+            } else if (grounded) {
+                SetVelocity(playerRigidbody.velocity.x, JumpPower);
+                slide(false);
+            }
+        }
+
+    }
+
+    private void keyboardMovement() {
         grounded = Physics2D.IsTouchingLayers(GetComponent<Collider2D>(), Ground);
 
         if (Input.GetKeyDown(KeyCode.UpArrow)){
             if (grounded) {
                 SetVelocity(playerRigidbody.velocity.x, JumpPower);
+                slide(false);
             }
         }
 
-        /*if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.localScale = new Vector3(transform.localScale.x, 1f, transform.localScale.y);
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            getUpTime = Time.timeSinceLevelLoad + SlideDuration;
+            slide(true);
+        }
+
+    }
+
+    private void slide(bool sliding) {
+        if (sliding) {
+            GetComponent<BoxCollider2D>().size = new Vector2(0.2f, 0.125f);
+            GetComponent<BoxCollider2D>().offset = new Vector2(0.015f, -0.1f);
         } else {
-            transform.localScale = new Vector3(transform.localScale.x, 3f, transform.localScale.y);
-        }*/
+            GetComponent<BoxCollider2D>().size = new Vector2(0.105f, 0.24f);
+            GetComponent<BoxCollider2D>().offset = new Vector2(0.01f, -0.04f);
+        }
+
+        this.sliding = sliding;
     }
 
     private void SetVelocity(float x, float y) {
